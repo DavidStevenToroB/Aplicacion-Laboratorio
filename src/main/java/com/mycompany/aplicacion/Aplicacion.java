d8114da4895ec13f9a1a41fbec2729a698756b47
SENA <SENA@ARMSCCCCTGOD072.FormacionSena.RED>
2024-10-11 14:35:12 -0500

    second commit

---
+++ src/main/java/com/mycompany/aplicacion/Aplicacion.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.aplicacion;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        
+            Entidad rectangulo = new Entidad ();
+            
+            rectangulo.ingresarDatos ();
+            rectangulo.calcularArea ();
+        }
+    }
+
